d271bb614b22213005038bdd4405c7e2f9ca7f24
LuckyLuke00 <lucas.kinoo5@gmail.com>
2022-11-23 17:40:20 +0100

    Remove redundant test functions

--- GP1_Rasterizer/source/Renderer.h
+++ GP1_Rasterizer/source/Renderer.h
@@ -33,13 +33,6 @@ namespace dae
 
 		void Render();
 
-		void Render_W1_Part1(); //Rasterizer Stage Only
-		void Render_W1_Part2(); //Projection Stage (Camera)
-		void Render_W1_Part3(); //Barycentric Coordinates
-		void Render_W1_Part4(); //Depth Buffer
-		void Render_W1_Part5(); //BoundingBox Optimization
-
-		void Render_W2(); //Textures
 		void Render_W3(); //Matrix Transformations
 
 		void ToggleDepthBuffer();
@@ -70,11 +63,8 @@ namespace dae
 		void ClearBuffers(const Uint8& r = 100, const Uint8& g = 100, const Uint8& b = 100);
 
 		//Function that transforms the vertices from the mesh from World space to Screen space
-		void VertexTransformationFunction(const std::vector<Vertex>& vertices_in, std::vector<Vertex>& vertices_out) const; //W1 Version
-		void VertexTransformationFunction(const std::vector<Mesh>& meshes_in, std::vector<Mesh>& meshes_out) const; //W2 Version
 		void VertexTransformationFunction(std::vector<Mesh>& meshes) const; //W3 Version
 
-		void RenderTriangle(const Vertex& v0, const Vertex& v1, const Vertex& v2, const Texture* pTexture = nullptr) const;
 		void RenderTriangle(const Vertex_Out& v0, const Vertex_Out& v1, const Vertex_Out& v2, const Texture* pTexture = nullptr) const;
 		void RenderMesh(const Mesh& mesh, const Texture* pTexture = nullptr) const;
 	};
